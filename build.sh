92b6ec1805be44590e91bda5e65e6d3ea3d86d64
brian urbina vargas <bria.urbina@duocuc.cl>
2023-06-21 00:02:43 -0400

    cambios build

--- build.sh
+++ build.sh
@@ -6,3 +6,10 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+python manage.py createsuperuser
+
+admin
+admin@admin.cl
+hola1234
+hola1234
